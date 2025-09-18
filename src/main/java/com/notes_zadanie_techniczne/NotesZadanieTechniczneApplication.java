7149655b653b328c80f1c8d9b57c04484b2e6402
LukaszCh233 <103947294+LukaszCh233@users.noreply.github.com>
2025-09-18 11:27:22 +0200

    Initial commit

---
+++ src/main/java/com/notes_zadanie_techniczne/NotesZadanieTechniczneApplication.java
@@ -0,0 +1,13 @@
+package com.notes_zadanie_techniczne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotesZadanieTechniczneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotesZadanieTechniczneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/notes_zadanie_techniczne/NotesZadanieTechniczneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.notes_zadanie_techniczne;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotesZadanieTechniczneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
